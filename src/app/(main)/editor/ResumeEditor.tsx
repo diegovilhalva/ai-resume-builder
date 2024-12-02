21a44779467fcf8dafaf21cf53ea538229ea2b03
Diego Vilhalva <diegorocha450@yahoo.com>
2024-12-02 14:03:14 -0300

    creating resume form editor

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -1,20 +1,40 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import React from 'react'
+import GeneralInfoForm from './forms/GeneralInfoForm'
+import PersonalInfoForm from './forms/PersonalInfoForm'
+import { useSearchParams } from 'next/navigation'
+import { steps } from './steps'
+import Breadcrumbs from './Breadcrumbs'
+import Footer from './Footer'
+
 
 const ResumeEditor = () => {
+    const searchParams  = useSearchParams()
+
+    const currentStep = searchParams.get('step') || steps[0].key
+    function setStep(key:string){
+        const newSearchParams = new URLSearchParams(searchParams)
+        newSearchParams.set("step",key)
+        window.history.pushState(null,"",`?${newSearchParams.toString()}`)
+    }
+
+    const FormComponent = steps.find(
+        step => step.key === currentStep
+    )?.component
     return (
         <div className='flex  grow  flex-col'>
             <header className='space-y-1.5 border-b px-3 py-5 text-center'>
-                <h1 className="text-2xl font-light">Design your resume</h1>
+                <h1 className="text-2xl font-bold">Design your resume</h1>
                 <p className="text-sm text-muted-foreground">
                     Follow the steps below to create your resume. Your progress will be saved automatically.
                 </p>
             </header>
             <main className='relative grow'>
                 <div className="absolute bottom-0 top-0 flex w-full">
-                    <div className="w-full  md:w-1/2 ">
-                    left
+                    <div className="w-full  md:w-1/2 p-3 overflow-y-auto  space-y-6">
+                        <Breadcrumbs currentStep={currentStep}  setCurrentStep={setStep} />
+                        {FormComponent && <FormComponent />}
                     </div>
                     <div className='grow md:border-r' />
                     <div className="hidden w-1/2 md:flex">
@@ -22,20 +42,7 @@ const ResumeEditor = () => {
                     </div>
                 </div>
             </main>
-            <footer className="w-full border-t px-3 py-5">
-                <div className="max-w-7xl mx-auto flex flex-wrap justify-between gap-3">
-                    <div className="flex items-center gap-3">
-                        <Button variant="secondary">Previous step</Button>
-                        <Button>Next step</Button>
-                    </div>
-                    <div className="flex items-center gap-3">
-                        <Button variant="secondary" asChild>
-                            <Link href="/resumes">Close</Link>
-                        </Button>
-                        <p className="text-muted-foreground opacity-0">Saving...</p>
-                    </div>
-                </div>
-            </footer>
+           <Footer currentStep={currentStep} setCurrentStep={setStep} />
         </div>
     )
 }
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -3,8 +3,23 @@ import { z } from "zod"
 export const optionalString = z.string().trim().optional().or(z.literal(""))
 
 export const generalInfoSchema = z.object({
-    title:optionalString,
-    description:optionalString
+    title: optionalString,
+    description: optionalString
 })
 
-export type GeneralInfoValues =  z.infer<typeof generalInfoSchema>
+export type GeneralInfoValues = z.infer<typeof generalInfoSchema>
+
+export const personalInfoSchema = z.object({
+    photo: z.custom<File | undefined>().refine((file) => !file || (file instanceof File && file.type.startsWith('image/')),
+        "Must be an image"
+    ).refine(file => !file || file.size <= 1014 * 1024 * 4, "Filer must be less than 4MB"),
+    firstName: optionalString,
+    lastName: optionalString,
+    jobTitle: optionalString,
+    city: optionalString,
+    country: optionalString,
+    phone: optionalString,
+    email: optionalString,
+})
+
+export type PersonalInfoValues = z.infer<typeof personalInfoSchema>
