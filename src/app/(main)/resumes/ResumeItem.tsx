521b3c8f842495fe4dd92f529df68708cf0d2502
Diego Vilhalva <diegorocha450@yahoo.com>
2024-12-09 11:46:09 -0300

    delting resumes

--- src/app/(main)/resumes/ResumeItem.tsx
+++ src/app/(main)/resumes/ResumeItem.tsx
@@ -1,9 +1,17 @@
 "use client"
 
 import ResumePreview from "@/components/ResumePreview"
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useToast } from "@/hooks/use-toast"
 import { mapToResumeValues, ResumeServerData } from "@/lib/types"
 import { formatDate } from "date-fns"
+import { MoreVertical, Trash2 } from "lucide-react"
 import Link from "next/link"
+import { useState, useTransition } from "react"
+import { deleteResume } from "./actions"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import LoadingButton from "@/components/LoadingButton"
 
 interface ResumeItemProps {
     resume: ResumeServerData
@@ -12,7 +20,7 @@ interface ResumeItemProps {
 const ResumeItem = ({ resume }: ResumeItemProps) => {
     const wasUpdated = resume.updatedAt !== resume.createdAt
     return (
-        <div className="group border rounde-lg rounded-lg border-transparent hover:border-border bg-secondary p-3">
+        <div className="group relative border rounde-lg rounded-lg border-transparent hover:border-border bg-secondary p-3">
             <div className="space-y-3">
                 <Link href={`/editor?resumeId=${resume.id}`} className="inline-block w-full text-center">
                     <p className="font-semibold line-clamp-1">{resume.title || 'No title'}</p>
@@ -20,8 +28,8 @@ const ResumeItem = ({ resume }: ResumeItemProps) => {
                         <p className="line-clamp-2 text-sm">{resume.description}</p>
                     )}
                     <p className="text-xs text-muted-foreground">
-                        {wasUpdated ? 'Updated' : 'Created'}  on{" "} 
-                        {formatDate(resume.updatedAt,"MMM d, yyyy h:mm a")}
+                        {wasUpdated ? 'Updated' : 'Created'}  on{" "}
+                        {formatDate(resume.updatedAt, "MMM d, yyyy h:mm a")}
                     </p>
                 </Link>
                 <Link href={`/editor?resumeId=${resume.id}`} className="inline-block w-full">
@@ -29,8 +37,81 @@ const ResumeItem = ({ resume }: ResumeItemProps) => {
                     <div className="abusolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-white to-transparent" />
                 </Link>
             </div>
+            <MoreMenu resumeId={resume.id} />
         </div>
     )
+}
+
+interface MoreMenuProps {
+    resumeId: string
+}
+
+const MoreMenu = ({ resumeId }: MoreMenuProps) => {
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100">
+                        <MoreVertical className="size-4" size="icon" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem className="flex items-center gap-2" onClick={() => setShowDeleteConfirmation(true)}>
+                        <Trash2 className="size-4" />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DeleteConfirmationDialog resumeId={resumeId} open={showDeleteConfirmation} onOpenChange={setShowDeleteConfirmation} />
+        </>
+    )
+}
+
+interface DeleteConfirmationDialogProps {
+    resumeId: string
+    open: boolean
+    onOpenChange: (open: boolean) => void
+}
+
+const DeleteConfirmationDialog = ({ resumeId, open, onOpenChange }: DeleteConfirmationDialogProps) => {
+    const { toast } = useToast()
+
+    const [isPending, startTransition] = useTransition()
+
+    async function handleDelete() {
+        startTransition(async () => {
+            try {
+                await deleteResume(resumeId)
+                onOpenChange(false)
+            } catch (error) {
+                toast({
+                    variant: "destructive",
+                    description: 'Something went wrong. Please try again'
+                })
+            }
+        })
+    }
+
+    return <Dialog open={open} onOpenChange={onOpenChange}>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Delete Resume?</DialogTitle>
+                <DialogDescription>
+                    This will permanently delete this resume. This action cannot be undone.
+                </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+                <LoadingButton variant='destructive' onClick={handleDelete} loading={isPending}>
+                    Delete
+                </LoadingButton>
+                <Button variant="secondary" onClick={() => onOpenChange(false)}>
+                    Cancel
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
 }
 
 export default ResumeItem
