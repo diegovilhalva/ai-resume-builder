1b440d866aa0beabe76b01f1a5c09113a76101ce
Diego Vilhalva <diegorocha450@yahoo.com>
2024-12-11 12:02:27 -0300

    correction at the billing page

--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -7,7 +7,7 @@ import GetSubscriptionButton from "./GetSubscriptionButton"
 import { formatDate } from "date-fns"
 import ManageSubscriptionButton from "./ManageSubscriptionButton"
 
-export const metada: Metadata = {
+export const metadata:Metadata = {
   title: "Billing"
 }
 
@@ -19,13 +19,13 @@ const Page = async () => {
   }
 
   const subscription = await prisma.userSubscription.findUnique({
-    where:{userId}
+    where: { userId }
   })
 
-  const priceInfo = subscription ? await stripe.prices.retrieve(subscription.stripePriceId,{
-    expand:["product"]
+  const priceInfo = subscription ? await stripe.prices.retrieve(subscription.stripePriceId, {
+    expand: ["product"]
   })
-  :null
+    : null
   return (
     <main className="max-w-7xl mx-auto w-full space-y-6 px-3 py-6">
       <h1 className="text-3xl font-bold">Billing</h1>
@@ -33,21 +33,21 @@ const Page = async () => {
         Your current plan:{" "}
         <span className="font-bold">{priceInfo ? (priceInfo.product as Stripe.Product).name : "free"}</span>
       </p>
-      
+
       {subscription ? (
         <>
           {subscription.stripeCancelAtPeriodEnd && (
             <p className="text-destructive">
-              Your subscription will ne cancelled on:{""} 
-              {formatDate(subscription.stripeCurrentPeriodEnd,"MMMM dd, yyyy")}
+              Your subscription will ne cancelled on:{""}
+              {formatDate(subscription.stripeCurrentPeriodEnd, "MMMM dd, yyyy")}
             </p>
           )}
           <ManageSubscriptionButton />
         </>
-      ): (
-        <GetSubscriptionButton/>
+      ) : (
+        <GetSubscriptionButton />
       )}
-      
+
     </main>
   )
 }
